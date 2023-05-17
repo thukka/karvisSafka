a5b64dbc1c6bf2ace573bd8285a864c594bf69aa
Hukka <toni.hukka@cgi.com>
2023-05-17 15:48:20 +0300

    removing the date depending on the URL returned from getData

--- src/app/page.js
+++ src/app/page.js
@@ -2,28 +2,33 @@ import styles from './page.module.css'
 import { getData } from './api/route';
 const cheerio = require('cheerio');
 
-const trimDay = arr => {
-  const modifiedArr = arr[1]
+const trimDay = (url, arr) => {
+  let modifiedArr = arr[1]
     .split(/(?![^(]*\))\b(?=[A-Z])/)
     .map(item => item.trim()).filter(item => item !== '')
+
+  if (url === process.env.FACTORY_URL) {
+    // 9 ensimmäistä merkkiä pitää poistaa
+    modifiedArr[0] = modifiedArr[0].substring(9)
+  } else {
+    modifiedArr = modifiedArr.slice(1);
+  }
 
   return modifiedArr;
 };
 
-const getDayLunchlist = body => {
-
-
-  const $ = cheerio.load(body);
+const getDayLunchlist = data => {
+  const $ = cheerio.load(data.body);
   const textTrimmed = $.text().replace(/(\r\n|\n|\r)/gm, "");
   const patternMonday = /Maanantai(.*?)Tiistai/;
   /* const patternTuesday = /Tiistai(.*?)Keskiviikko/; */
   const result = textTrimmed.match(patternMonday);
-  return trimDay(result)
+  return trimDay(data.url, result)
 }
 
 
 export default async function Home() {
-  const data = await getData(process.env.MAPLESTR_URL)
+  const data = await getData(process.env.FACTORY_URL)
   const lunchArr = getDayLunchlist(data)
   console.log('lunchArr: ', lunchArr)
 
